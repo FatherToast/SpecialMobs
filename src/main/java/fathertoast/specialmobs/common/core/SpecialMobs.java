0dd7bcc15851e111ba05dc21f4856f5d21e3a7cc
Sarinsa <mikkemusbruker@gmail.com>
2022-06-22 06:30:31 +0200

    Lol

--- src/main/java/fathertoast/specialmobs/common/core/SpecialMobs.java
+++ src/main/java/fathertoast/specialmobs/common/core/SpecialMobs.java
@@ -4,7 +4,9 @@ import fathertoast.specialmobs.common.bestiary.MobFamily;
 import fathertoast.specialmobs.common.config.Config;
 import fathertoast.specialmobs.common.core.register.SMEntities;
 import fathertoast.specialmobs.common.core.register.SMItems;
+import fathertoast.specialmobs.common.event.BiomeEvents;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -101,7 +103,8 @@ public class SpecialMobs {
         //packetHandler.registerMessages();
         
         //MinecraftForge.EVENT_BUS.register( new SMEventListener() );
-        
+        MinecraftForge.EVENT_BUS.register( new BiomeEvents() );
+
         IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
         
         eventBus.addListener( SMEntities::createAttributes );
