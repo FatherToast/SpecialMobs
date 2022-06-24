99a494923b531fff377cfe7bec2837da6b7cc4fc
Sarinsa <mikkemusbruker@gmail.com>
2022-06-24 05:32:27 +0200

    Syringe tweaks

--- src/main/java/fathertoast/specialmobs/common/item/SyringeItem.java
+++ src/main/java/fathertoast/specialmobs/common/item/SyringeItem.java
@@ -4,6 +4,7 @@ import fathertoast.specialmobs.common.bestiary.SpecialMob;
 import fathertoast.specialmobs.common.util.References;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Rarity;
 import net.minecraft.potion.EffectInstance;
@@ -14,7 +15,7 @@ import net.minecraft.world.World;
 public class SyringeItem extends Item {
 
     public SyringeItem() {
-        super(new Item.Properties().stacksTo(1).rarity(Rarity.UNCOMMON).defaultDurability(10).setNoRepair());
+        super(new Item.Properties().stacksTo(1).rarity(Rarity.UNCOMMON).defaultDurability(10).setNoRepair().tab(ItemGroup.TAB_MISC));
     }
 
     @SpecialMob.LanguageProvider
@@ -28,19 +29,27 @@ public class SyringeItem extends Item {
         ItemStack usedItem = player.getItemInHand(hand);
 
         if (!world.isClientSide) {
-            if (player.hasEffect(Effects.MOVEMENT_SPEED)) {
+            if (player.getCooldowns().isOnCooldown(usedItem.getItem())) {
                 return ActionResult.pass(usedItem);
             }
             else {
                 player.addEffect(new EffectInstance(Effects.MOVEMENT_SPEED, 300, 2));
+                player.addEffect(new EffectInstance(Effects.DOLPHINS_GRACE, 300));
                 player.addEffect(new EffectInstance(Effects.CONFUSION, 400));
                 world.playSound(null, player.getX(), player.getY(), player.getZ(), SoundEvents.BEE_STING, SoundCategory.PLAYERS, 0.9F, 1.0F);
                 usedItem.hurtAndBreak(1, player, (entity) -> {
                     entity.broadcastBreakEvent(hand);
                 });
+                player.getCooldowns().addCooldown(usedItem.getItem(), 1200);
                 return ActionResult.success(usedItem);
             }
         }
         return ActionResult.fail(usedItem);
+    }
+
+    // Not enchantable
+    @Override
+    public boolean isEnchantable(ItemStack itemStack) {
+        return false;
     }
 }
