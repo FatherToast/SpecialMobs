214bd23b922ae499103f3d9fc87b305769f1ce2f
FatherToast <father.toast@hotmail.com>
2022-06-30 12:40:43 -0500

    Spitfire mobs

--- src/main/java/fathertoast/specialmobs/common/bestiary/MobFamily.java
+++ src/main/java/fathertoast/specialmobs/common/bestiary/MobFamily.java
@@ -55,11 +55,11 @@ public class MobFamily<T extends LivingEntity> {
     
     public static final MobFamily<AbstractSkeletonEntity> SKELETON = new MobFamily<>(
             "Skeleton", "skeletons", 0xC1C1C1, new EntityType[] { EntityType.SKELETON, EntityType.STRAY },
-            "Brute", "Fire", "Gatling", "Giant", "Knight", "Ninja", "Poison", "Sniper", /*"Spitfire",*/ "Stray"
+            "Brute", "Fire", "Gatling", "Giant", "Knight", "Ninja", "Poison", "Sniper", "Spitfire", "Stray"
     );
     public static final MobFamily<AbstractSkeletonEntity> WITHER_SKELETON = new MobFamily<>(
             "WitherSkeleton", "wither skeletons", 0x141414, new EntityType[] { EntityType.WITHER_SKELETON },
-            "Brute", "Gatling", "Giant", "Knight", "Ninja", "Sniper"//, "Spitfire"
+            "Brute", "Gatling", "Giant", "Knight", "Ninja", "Sniper", "Spitfire"
     );
     
     public static final MobFamily<SlimeEntity> SLIME = new MobFamily<>(
