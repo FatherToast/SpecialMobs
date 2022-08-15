735fde27f8a71a803d36d5c4b2290f3f2f1bfd37
FatherToast <father.toast@hotmail.com>
2022-08-15 17:53:50 -0500

    Fix hurt color

--- src/main/java/fathertoast/specialmobs/client/renderer/entity/layers/SpecialMobOverlayLayer.java
+++ src/main/java/fathertoast/specialmobs/client/renderer/entity/layers/SpecialMobOverlayLayer.java
@@ -1,21 +1,18 @@
 package fathertoast.specialmobs.client.renderer.entity.layers;
 
 import com.mojang.blaze3d.matrix.MatrixStack;
-import com.mojang.blaze3d.vertex.IVertexBuilder;
 import fathertoast.specialmobs.common.entity.ISpecialMob;
 import net.minecraft.client.renderer.IRenderTypeBuffer;
-import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.IEntityRenderer;
 import net.minecraft.client.renderer.entity.layers.LayerRenderer;
 import net.minecraft.client.renderer.entity.model.EntityModel;
-import net.minecraft.client.renderer.texture.OverlayTexture;
-import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
 @OnlyIn( Dist.CLIENT )
-public class SpecialMobOverlayLayer<T extends Entity, M extends EntityModel<T>> extends LayerRenderer<T, M> {
+public class SpecialMobOverlayLayer<T extends LivingEntity, M extends EntityModel<T>> extends LayerRenderer<T, M> {
     
     private final M layerModel;
     
@@ -27,17 +24,11 @@ public class SpecialMobOverlayLayer<T extends Entity, M extends EntityModel<T>> 
     @Override
     public void render( MatrixStack matrixStack, IRenderTypeBuffer buffer, int packedLight, T entity,
                         float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch ) {
-        if( entity.isInvisible() ) return;
-        
         final ResourceLocation overlayTexture = ((ISpecialMob<?>) entity).getSpecialData().getTextureOverlay();
         if( overlayTexture == null ) return;
         
-        getParentModel().copyPropertiesTo( layerModel );
-        layerModel.prepareMobModel( entity, limbSwing, limbSwingAmount, partialTicks );
-        layerModel.setupAnim( entity, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch );
-        
-        final IVertexBuilder vertexBuilder = buffer.getBuffer( RenderType.entityCutoutNoCull( overlayTexture ) );
-        layerModel.renderToBuffer( matrixStack, vertexBuilder, packedLight, OverlayTexture.NO_OVERLAY,
-                1.0F, 1.0F, 1.0F, 1.0F ); // RGBA
+        coloredCutoutModelCopyLayerRender( getParentModel(), layerModel, overlayTexture, matrixStack, buffer,
+                packedLight, entity, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, partialTicks,
+                1.0F, 1.0F, 1.0F ); // RGB
     }
 }
