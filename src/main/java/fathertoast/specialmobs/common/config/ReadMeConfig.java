8c7f78eb3109905ae32b9bdcc6891d92ec1bb82a
FatherToast <father.toast@hotmail.com>
2024-08-20 19:13:45 -0500

    Prep for le update

--- src/main/java/fathertoast/specialmobs/common/config/ReadMeConfig.java
+++ src/main/java/fathertoast/specialmobs/common/config/ReadMeConfig.java
@@ -18,8 +18,8 @@ public class ReadMeConfig extends AbstractConfigFile {
                 " * Family              - The group of mobs based on (but not including) a particular vanilla mob; e.g., Creepers.",
                 " * Species             - A specific type of mob within a family; e.g., Fire Creepers or vanilla-replacement Creepers.",
                 " * Vanilla Replacement - The one species within a family that is intended to be a replica of the base vanilla mob.",
-                " * Special Variant     - Any species that is not the family's vanilla replacement. Includes species that are",
-                "                         replicas of 'vanilla special variants'; i.e. Husks and Strays.",
+                " * Special Variant     - Any species that is not the family's vanilla replacement. Includes species that are " +
+                        "replicas of 'vanilla special variants'; i.e. Husks and Strays.",
                 " * Mob Replacer        - The tool that watches vanilla mob spawns and cancels them to spawn this mod's entities." );
         SPEC.newLine( 2 );
         SPEC.describeAttributeList();
@@ -30,7 +30,7 @@ public class ReadMeConfig extends AbstractConfigFile {
         SPEC.newLine();
         SPEC.describeEnvironmentListPart2of2();
         
-        SPEC.newLine( 4 );
+        SPEC.newLine( 6 );
         SPEC.define( new BooleanField( "secret_mode", false, (String[]) null ) );
     }
 }
