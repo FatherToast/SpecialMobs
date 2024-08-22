7a39569977c51a6024074e6e4afee5fdb46bf36c
FatherToast <father.toast@hotmail.com>
2024-08-22 09:45:23 -0500

    RIP armored blaze realistic rain effects

--- src/main/java/fathertoast/specialmobs/common/entity/blaze/ArmoredBlazeEntity.java
+++ src/main/java/fathertoast/specialmobs/common/entity/blaze/ArmoredBlazeEntity.java
@@ -29,13 +29,12 @@ import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
 
 public class ArmoredBlazeEntity extends _SpecialBlazeEntity {
-
-
+    
     //--------------- Static Special Mob Hooks ----------------
-
+    
     @SpecialMob.SpeciesReference
     public static MobFamily.Species<ArmoredBlazeEntity> SPECIES;
-
+    
     @SpecialMob.BestiaryInfoSupplier
     public static void getBestiaryInfo( BestiaryInfo.Builder bestiaryInfo ) {
         bestiaryInfo.color( 0xE5E5E5 )
@@ -45,117 +44,112 @@ public class ArmoredBlazeEntity extends _SpecialBlazeEntity {
                 .fireballAttack( 0, 80, 120, 40.0 )
                 .addToAttribute( Attributes.MAX_HEALTH, 15.0 );
     }
-
+    
     @SpecialMob.ConfigSupplier
-    public static SpeciesConfig createConfig(ConfigManager manager, MobFamily.Species<?> species ) {
+    public static SpeciesConfig createConfig( ConfigManager manager, MobFamily.Species<?> species ) {
         return new BlazeSpeciesConfig( manager, species, 5, 10 );
     }
-
+    
     @SpecialMob.LanguageProvider
     public static String[] getTranslations( String langKey ) {
         return References.translations( langKey, "Armored Blaze",
                 "", "", "", "", "", "" );//TODO
     }
-
+    
     @SpecialMob.LootTableProvider
     public static void buildLootTable( LootTableBuilder loot ) {
         addBaseLoot( loot );
         loot.addCommonDrop( "common", Items.BLAZE_ROD );
-        loot.addSemicommonDrop("semicommon", Items.IRON_INGOT);
+        loot.addSemicommonDrop( "semicommon", Items.IRON_INGOT );
     }
-
+    
     @SpecialMob.Factory
     public static EntityType.EntityFactory<ArmoredBlazeEntity> getVariantFactory() { return ArmoredBlazeEntity::new; }
-
+    
     /** @return This entity's mob species. */
     @SpecialMob.SpeciesSupplier
     @Override
     public MobFamily.Species<? extends ArmoredBlazeEntity> getSpecies() { return SPECIES; }
-
-
+    
+    
     //--------------- Variant-Specific Implementations ----------------
-
+    
     public static final EntityDataAccessor<Integer> ARMOR_LEVEL = SynchedEntityData.defineId( ArmoredBlazeEntity.class, EntityDataSerializers.INT );
-
+    
     /** The base explosion strength of this blaze's fireballs. */
     private int explosionPower = 2;
-
+    
     public ArmoredBlazeEntity( EntityType<? extends _SpecialBlazeEntity> entityType, Level level ) { super( entityType, level ); }
-
+    
     @Override
     protected void defineSynchedData() {
         super.defineSynchedData();
         entityData.define( ARMOR_LEVEL, 7 );
     }
-
+    
     public boolean hasArmor() {
         return entityData.get( ARMOR_LEVEL ) > 0;
     }
-
+    
     /** @return Attempts to damage this entity; returns true if the hit was successful. */
     @Override
     public boolean hurt( DamageSource source, float amount ) {
-        if ( entityData.get( ARMOR_LEVEL ) > 0 ) {
+        if( entityData.get( ARMOR_LEVEL ) > 0 ) {
             // Make sure invul bypass damage actually does damage before
             // we do anything else
-            if ( source.is( DamageTypeTags.BYPASSES_INVULNERABILITY ) ) {
-                return super.hurt( source, amount);
+            if( source.is( DamageTypeTags.BYPASSES_INVULNERABILITY ) ) {
+                return super.hurt( source, amount );
             }
             // Check if damage type can damage the blaze's armor
-            if ( !source.is( SMTags.IS_MAGIC ) && !source.is( DamageTypeTags.IS_PROJECTILE ) ) {
+            if( !source.is( SMTags.IS_MAGIC ) && !source.is( DamageTypeTags.IS_PROJECTILE ) ) {
                 // Do not damage the armor if the amount is less than 2.0
-                if ( amount >= 2.0F ) {
+                if( amount >= 2.0F ) {
                     int newArmorLevel = entityData.get( ARMOR_LEVEL ) - 1;
                     entityData.set( ARMOR_LEVEL, newArmorLevel );
-
+                    
                     // If armor is depleted, play some cool effects
-                    if ( newArmorLevel <= 0 ) {
-                        if ( !level().isClientSide ) {
-                            ( (ServerLevel) level() ).sendParticles(
-                                    new ItemParticleOption(ParticleTypes.ITEM, new ItemStack( Items.IRON_HELMET ) ),
-                                    getX() + 0.5D,
-                                    getY() + 0.5D,
-                                    getZ() + 0.5D,
-                                    5,
-                                    0,
-                                    0,
-                                    0,
-                                    0.2
-                            );
+                    if( newArmorLevel <= 0 ) {
+                        if( !level().isClientSide ) {
+                            ((ServerLevel) level()).sendParticles(
+                                    new ItemParticleOption( ParticleTypes.ITEM, new ItemStack( Items.IRON_HELMET ) ),
+                                    getX() + 0.5D, getY() + 0.5D, getZ() + 0.5D,
+                                    5, 0, 0, 0, 0.2 );
                             playSound( SoundEvents.ITEM_BREAK );
                         }
                     }
+                    else {
+                        playSound( SoundEvents.ANVIL_PLACE, 0.8F, 0.8F + level().random.nextFloat() * 0.4F );
+                    }
                 }
             }
-            Entity attacker = source.getDirectEntity();
-
+            
             // Try knocking back attacker, if there is a direct attacker
-            if ( attacker instanceof LivingEntity livingEntity ) {
+            Entity attacker = source.getDirectEntity();
+            if( attacker instanceof LivingEntity livingEntity ) {
                 livingEntity.knockback( 0.5F,
                         getX() - attacker.getX(), getZ() - attacker.getZ() );
             }
-            playSound( SoundEvents.ANVIL_PLACE, 0.8F, 0.8F + level().random.nextFloat() * 0.4F );
             return false;
         }
         else {
             return super.hurt( source, amount );
         }
     }
-
+    
     /** Override to save data to this entity's NBT data. */
     @Override
     public void addVariantSaveData( CompoundTag saveTag ) {
         saveTag.putByte( References.TAG_EXPLOSION_POWER, (byte) explosionPower );
         saveTag.putInt( "ArmorLevel", entityData.get( ARMOR_LEVEL ) );
     }
-
+    
     /** Override to load data from this entity's NBT data. */
     @Override
     public void readVariantSaveData( CompoundTag saveTag ) {
         if( saveTag.contains( References.TAG_EXPLOSION_POWER, References.NBT_TYPE_NUMERICAL ) )
             explosionPower = saveTag.getByte( References.TAG_EXPLOSION_POWER );
-
-        if ( saveTag.contains( "ArmorLevel", Tag.TAG_INT ) ) {
+        
+        if( saveTag.contains( "ArmorLevel", Tag.TAG_INT ) ) {
             entityData.set( ARMOR_LEVEL, saveTag.getInt( "ArmorLevel" ) );
         }
     }
