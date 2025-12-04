257b44618bc9d6d39b8028adac56bf51f5eeb328
Sarinsa <mikkemusbruker@gmail.com>
2025-12-04 01:23:02 +0100

    Updated ReadMeConfig to be more readable in the config GUI

--- src/main/java/fathertoast/specialmobs/common/config/ReadMeConfig.java
+++ src/main/java/fathertoast/specialmobs/common/config/ReadMeConfig.java
@@ -3,34 +3,43 @@ package fathertoast.specialmobs.common.config;
 import fathertoast.crust.api.config.common.AbstractConfigFile;
 import fathertoast.crust.api.config.common.ConfigManager;
 import fathertoast.crust.api.config.common.field.BooleanField;
+import net.minecraft.ChatFormatting;
 
 public class ReadMeConfig extends AbstractConfigFile {
-
+    
     public final BooleanField secretMode;
-
+    
     /** Builds the config spec that should be used for this config. */
     protected ReadMeConfig( ConfigManager manager ) {
         super( manager, "README", "This file contains helpful information about how to use the config files in this mod." );
         SPEC.newLine( 2 );
-        SPEC.comment(
-                "Terminology used in Special Mobs configs:",
-                " * Mob                 - An entity that is 'alive', short for \"Mobile\" or MobEntity.",
-                " * Family              - The group of mobs based on (but not including) a particular vanilla mob; e.g., Creepers.",
-                " * Species             - A specific type of mob within a family; e.g., Fire Creepers or vanilla-replacement Creepers.",
-                " * Vanilla Replacement - The one species within a family that is intended to be a replica of the base vanilla mob.",
-                " * Special Variant     - Any species that is not the family's vanilla replacement. Includes species that are " +
-                        "replicas of 'vanilla special variants'; i.e. Husks and Strays.",
-                " * Mob Replacer        - The tool that watches vanilla mob spawns and cancels them to spawn this mod's entities." );
-        SPEC.newLine( 2 );
-        SPEC.describeAttributeList();
-        SPEC.newLine( 2 );
-        SPEC.describeRegistryEntryList();
-        SPEC.newLine( 2 );
-        SPEC.describeEnvironmentListPart1of2();
-        SPEC.newLine();
-        SPEC.describeEnvironmentListPart2of2();
+        SPEC.comment( ChatFormatting.AQUA + "Terminology used in Special Mobs configs:" );
         
-        SPEC.newLine( 6 );
+        SPEC.newLine();
+        SPEC.comment( ChatFormatting.YELLOW + " * Mob " + ChatFormatting.GRAY +
+                "- An entity that is 'alive', short for \"Mobile\" or MobEntity." );
+        
+        SPEC.newLine();
+        SPEC.comment( ChatFormatting.YELLOW + " * Family " + ChatFormatting.GRAY +
+                "- The group of mobs based on (but not including) a particular vanilla mob; e.g., Creepers." );
+        
+        SPEC.newLine();
+        SPEC.comment( ChatFormatting.YELLOW + " * Species " + ChatFormatting.GRAY +
+                "- A specific type of mob within a family; e.g., Fire Creepers or vanilla-replacement Creepers." );
+        
+        SPEC.newLine();
+        SPEC.comment( ChatFormatting.YELLOW + " * Vanilla Replacement " + ChatFormatting.GRAY +
+                "- The one species within a family that is intended to be a replica of the base vanilla mob." );
+        
+        SPEC.newLine();
+        SPEC.comment( ChatFormatting.YELLOW + " * Special Variant " + ChatFormatting.GRAY +
+                "- Any species that is not the family's vanilla replacement. Includes species that are replicas of 'vanilla special variants'; i.e. Husks and Strays." );
+        
+        SPEC.newLine();
+        SPEC.comment( ChatFormatting.YELLOW + " * Mob Replacer " + ChatFormatting.GRAY +
+                "- The tool that watches vanilla mob spawns and cancels them to spawn this mod's entities." );
+        
+        SPEC.newLine( 8 );
         secretMode = SPEC.define( new BooleanField( "secret_mode", false, (String[]) null ) );
     }
 }
