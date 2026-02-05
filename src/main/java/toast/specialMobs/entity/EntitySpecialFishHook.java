e7c74a93f839ccb5ef4523279d35d94b3b3cb26d
Sarinsa <mikkemusbruker@gmail.com>
2026-02-05 22:06:06 +0100

    Bumped mod version

--- src/main/java/toast/specialMobs/entity/EntitySpecialFishHook.java
+++ src/main/java/toast/specialMobs/entity/EntitySpecialFishHook.java
@@ -207,13 +207,14 @@ public class EntitySpecialFishHook extends Entity {
             double v = Math.sqrt( vX * vX + vY * vY + vZ * vZ );
             double mult = 0.31;
             
-            if( EntitySpecialFishHook.TROLLING && object.entityHit instanceof EntityPlayer && "1337Fenix".equals( ((EntityPlayer) object.entityHit).getCommandSenderName() ) ) {
+            if( EntitySpecialFishHook.TROLLING && object.entityHit instanceof EntityPlayer && "1337Fenix".equals( object.entityHit.getCommandSenderName() ) ) {
                 mult *= 2.0;
             }
             object.entityHit.motionX = vX * mult;
             object.entityHit.motionY = vY * mult + Math.sqrt( v ) * 0.1;
             object.entityHit.motionZ = vZ * mult;
             object.entityHit.onGround = false;
+            
             if( object.entityHit instanceof EntityPlayerMP ) {
                 try {
                     ((EntityPlayerMP) object.entityHit).playerNetServerHandler.sendPacket( new S12PacketEntityVelocity( object.entityHit ) );
