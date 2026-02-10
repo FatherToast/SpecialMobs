b6a14404a07bfdd3b2ef410dfbd0fe5c65a7a8a6
Sarinsa <mikkemusbruker@gmail.com>
2026-02-10 16:25:16 +0100

    Cleaned up SMTags

--- src/main/java/fathertoast/specialmobs/common/core/register/SMTags.java
+++ src/main/java/fathertoast/specialmobs/common/core/register/SMTags.java
@@ -13,34 +13,32 @@ public class SMTags {
     // Damage types
     public static class DamageTypes {
         
-        public static final TagKey<DamageType> IS_MAGIC = TagKey.create( Registries.DAMAGE_TYPE, ResourceLocation.fromNamespaceAndPath( "forge", "is_magic" ) );
-        
+        public static final TagKey<DamageType> IS_MAGIC = TagKey.create( Registries.DAMAGE_TYPE, forge( "is_magic" ) );
     }
     
     // Item tags
     public static class Items {
         
-        public static final TagKey<Item> GARLIC = ItemTags.create( ResourceLocation.fromNamespaceAndPath( "forge", "vegetables/garlic" ) );
-        
+        public static final TagKey<Item> GARLIC = ItemTags.create( forge( "vegetables/garlic" ) );
     }
     
     // Entity type tags
     public static class EntityTypes {
         
-        public static final TagKey<EntityType<?>> BLAZES = new TagKey<>( Registries.ENTITY_TYPE, forge( "blazes" ) );
-        public static final TagKey<EntityType<?>> CAVE_SPIDERS = new TagKey<>( Registries.ENTITY_TYPE, forge( "cave_spiders" ) );
-        public static final TagKey<EntityType<?>> CREEPERS = new TagKey<>( Registries.ENTITY_TYPE, forge( "creepers" ) );
-        public static final TagKey<EntityType<?>> DROWNED = new TagKey<>( Registries.ENTITY_TYPE, forge( "drowned" ) );
-        public static final TagKey<EntityType<?>> ENDERMEN = new TagKey<>( Registries.ENTITY_TYPE, forge( "endermen" ) );
-        public static final TagKey<EntityType<?>> GHASTS = new TagKey<>( Registries.ENTITY_TYPE, forge( "ghasts" ) );
-        public static final TagKey<EntityType<?>> MAGMA_CUBES = new TagKey<>( Registries.ENTITY_TYPE, forge( "magma_cubes" ) );
-        public static final TagKey<EntityType<?>> SILVERFISH = new TagKey<>( Registries.ENTITY_TYPE, forge( "silverfish" ) );
-        public static final TagKey<EntityType<?>> SLIMES = new TagKey<>( Registries.ENTITY_TYPE, forge( "slimes" ) );
-        public static final TagKey<EntityType<?>> SPIDERS = new TagKey<>( Registries.ENTITY_TYPE, forge( "spiders" ) );
-        public static final TagKey<EntityType<?>> WITCHES = new TagKey<>( Registries.ENTITY_TYPE, forge( "witches" ) );
-        public static final TagKey<EntityType<?>> WITHER_SKELETONS = new TagKey<>( Registries.ENTITY_TYPE, forge( "wither_skeletons" ) );
-        public static final TagKey<EntityType<?>> ZOMBIES = new TagKey<>( Registries.ENTITY_TYPE, forge( "zombies" ) );
-        public static final TagKey<EntityType<?>> ZOMBIFIED_PIGLINS = new TagKey<>( Registries.ENTITY_TYPE, forge( "zombified_piglins" ) );
+        public static final TagKey<EntityType<?>> BLAZES = TagKey.create( Registries.ENTITY_TYPE, forge( "blazes" ) );
+        public static final TagKey<EntityType<?>> CAVE_SPIDERS = TagKey.create( Registries.ENTITY_TYPE, forge( "cave_spiders" ) );
+        public static final TagKey<EntityType<?>> CREEPERS = TagKey.create( Registries.ENTITY_TYPE, forge( "creepers" ) );
+        public static final TagKey<EntityType<?>> DROWNED = TagKey.create( Registries.ENTITY_TYPE, forge( "drowned" ) );
+        public static final TagKey<EntityType<?>> ENDERMEN = TagKey.create( Registries.ENTITY_TYPE, forge( "endermen" ) );
+        public static final TagKey<EntityType<?>> GHASTS = TagKey.create( Registries.ENTITY_TYPE, forge( "ghasts" ) );
+        public static final TagKey<EntityType<?>> MAGMA_CUBES = TagKey.create( Registries.ENTITY_TYPE, forge( "magma_cubes" ) );
+        public static final TagKey<EntityType<?>> SILVERFISH = TagKey.create( Registries.ENTITY_TYPE, forge( "silverfish" ) );
+        public static final TagKey<EntityType<?>> SLIMES = TagKey.create( Registries.ENTITY_TYPE, forge( "slimes" ) );
+        public static final TagKey<EntityType<?>> SPIDERS = TagKey.create( Registries.ENTITY_TYPE, forge( "spiders" ) );
+        public static final TagKey<EntityType<?>> WITCHES = TagKey.create( Registries.ENTITY_TYPE, forge( "witches" ) );
+        public static final TagKey<EntityType<?>> WITHER_SKELETONS = TagKey.create( Registries.ENTITY_TYPE, forge( "wither_skeletons" ) );
+        public static final TagKey<EntityType<?>> ZOMBIES = TagKey.create( Registries.ENTITY_TYPE, forge( "zombies" ) );
+        public static final TagKey<EntityType<?>> ZOMBIFIED_PIGLINS = TagKey.create( Registries.ENTITY_TYPE, forge( "zombified_piglins" ) );
         
     }
     
