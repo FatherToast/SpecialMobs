d0201cdfa14b47e9eaf292b88863f0b92cb7e908
Sarinsa <mikkemusbruker@gmail.com>
2026-02-23 16:55:04 +0100

    WIP thingy

---
+++ src/main/java/fathertoast/specialmobs/common/compat/tc/TCMaterialHelper.java
@@ -0,0 +1,11 @@
+package fathertoast.specialmobs.common.compat.tc;
+
+import net.minecraft.world.item.ItemStack;
+
+public class TCMaterialHelper {
+    
+    // TODO
+    public static boolean isWoodenTool( ItemStack itemStack ) {
+        return false;
+    }
+}
